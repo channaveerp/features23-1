4f588d6a767edd3c7d574d9ac0cf1341d64a0221
channaveer <117071992+channaveer6925@users.noreply.github.com>
2023-02-22 12:51:48 +0530

    form updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Contactus from "./component";
 
 function App() {
   return (
     <div className="App">
-      <h1>lets start</h1>
+      {/* <h1>lets start</h1> */}
+      <Contactus />
     </div>
   );
 }
