829f2eede9a71a10fb241e851a149dcfd5c4e790
channaveer <channaveerpatil999@gmail.com>
2023-03-10 08:58:01 +0530

    Basic search functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
-import Contactus from "./component/form";
-import { Tabs } from "./component/tabs";
+import './App.css';
+import Contactus from './component/form';
+import { Tabs } from './component/tabs';
+import Search from './search_funct';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       {/* <h1>lets start</h1> */}
       {/* <Contactus /> */}
-      <Tabs />
+      {/* <Tabs /> */}
+      <Search />
     </div>
   );
 }
---
+++ src/component/TimeDate()/Data.js
@@ -0,0 +1,7 @@
+const data = [
+    {
+        name:"channa",
+        place:"India",
+        date:""
+    }
+]
