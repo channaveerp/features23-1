008a0a5190367f8a6bcd84cae9223d7f8d09f789
Channaveer Patil <97447924+channaveerp@users.noreply.github.com>
2023-05-26 10:04:23 +0530

    Create email.jsx
    email sending to

---
+++ src/component/email.jsx
@@ -0,0 +1,6 @@
+   <a
+                  target='_blank'
+                  href='https://mail.google.com/mail/?view=cm&fs=1&to=support@ajnalens.com&su=hello'
+                >
+                  Help
+                </a>
